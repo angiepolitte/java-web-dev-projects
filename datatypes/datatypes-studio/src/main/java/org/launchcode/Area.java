30fcb963ce2b62d793ebcc13a4b481ccf8f67401
Angie <angiepolitte@gmail.com>
2024-09-20 06:30:38 -0500

    Part of Bonus Mission

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -10,7 +10,13 @@ public class Area {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a radius: ");
         radius = input.nextDouble();
+        if (radius > 0) {
+            System.out.println("The area of a circle with radius " + radius + " is " + Circle.getArea(radius));
+        } else {
+            System.out.println("Please enter a positive number");
 
-        System.out.println("The area of a circle with radius " + radius + " is " + Circle.getArea(radius));
+        }
+
+
     }
 }
